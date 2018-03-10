8a3d3e4c28a074b7108ce8a79308168751e01dc1
Azelor <kaarel.soot@gmail.com>
2018-03-10 23:25:02 +0200

    Initial Spring Boot setup

---
+++ sportrent-rest/src/main/java/io/sportrent/SportrentRestApplication.java
@@ -0,0 +1,12 @@
+package io.sportrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportrentRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportrentRestApplication.class, args);
+	}
+}
